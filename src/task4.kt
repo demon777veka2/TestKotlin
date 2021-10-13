f9b934cb1b98113fc9494e0c271537c56ec4fe9a
demon777veka2 <58589191+demon777veka2@users.noreply.github.com>
2021-10-13 16:36:44 +0700

    В задании 5 убраны лишние переменные

--- src/task4.kt
+++ src/task4.kt
@@ -1,3 +1,3 @@
 fun main(args: Array<String>) {
-     args.sorted().groupingBy { it }.eachCount().forEach{it.key + " " + it.value}
+     args.sorted().groupingBy { it }.eachCount().forEach{println(it.key + " " + it.value)}
 }
--- src/task5.kt
+++ src/task5.kt
@@ -1,17 +1,11 @@
 fun main(args: Array<String>) {
-    // если через параметр ничего не пришло, передаем новые значения через ReadLine
-    var args: Array<String> = if (args.isEmpty()) {
-        println("Введите текст")
+    val text = if (args.isEmpty()) {
         readLine().toString().split(" ").toTypedArray()
     } else {
         args
     }
 
-    val mapCollection = args.sorted().groupingBy { it }.eachCount()
-    //Сортировка коллекции по количеству повторений на уменьшение и алфавиту
-    val result = mapCollection.toList().sortedByDescending { (_, value) -> value }.toMap()
-
-    for (i in result) {
-        println(i.key + " " + i.value)
-    }
+    text.sorted().groupingBy { it }.eachCount()
+            .toList().sortedByDescending { (_, value) -> value }.toMap()
+            .forEach { println(it.key + " " + it.value) }
 }
