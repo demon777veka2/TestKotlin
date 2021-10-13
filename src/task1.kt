dff75bb108e4aeb6a8aa55e0df64b3f57675109c
demon777veka2 <58589191+demon777veka2@users.noreply.github.com>
2021-10-13 10:38:50 +0700

    Убрана лишняя переменная в 1 задании

--- src/task1.kt
+++ src/task1.kt
@@ -1,9 +1,7 @@
 fun main(args: Array<String>) {
-    var list: Array<String> = args
-
     println()
     println("Задание 1")
     println("Результат:")
 
-    list.forEach(::println)
+    args.forEach(::println)
 }
