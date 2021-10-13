13aea9815d4a40d413938491727ed6e36aeb5eaa
demon777veka2 <58589191+demon777veka2@users.noreply.github.com>
2021-10-13 10:43:57 +0700

    Убрана лишняя переменная в 4 задании

--- src/task4.kt
+++ src/task4.kt
@@ -1,11 +1,9 @@
 fun main(args: Array<String>) {
-    var list: Array<String> = args
-
     println()
     println("Задание 4")
     println("Результат:")
 
-    val mapCollection = list.sorted().groupingBy { it }.eachCount()
+    val mapCollection = args.sorted().groupingBy { it }.eachCount()
 
     for (i in mapCollection) {
         println(i.key + " " + i.value)
